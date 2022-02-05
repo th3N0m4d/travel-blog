1008590f57de08f3c80926071133cf336dd3a7f3
th3N0m4d <edielton.dantas@hotmail.com>
2022-02-05 19:00:06 +0100

    feat(NavBar): add component

---
+++ client/components/layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import NavBar from "./navbar";
+
+export default function Layout({ children }) {
+  return (
+    <div className="container">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Oswald:400,500,700%7CRoboto:400,500,700%7CHerr+Von+Muellerhoff:400,500,700%7CQuattrocento+Sans:400,500,700"
+          type="text/css"
+          media="all"
+        />
+      </Head>
+      <NavBar />
+      <main className="site-content">{children}</main>
+    </div>
+  );
+}
